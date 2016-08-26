bea82b56d12b2b12b0d4820093a250d1d4a24010
Ishan <ishanatmuzaffarpur@gmail.com>
2016-08-27 04:32:31 +0530

    added comments, logging only URLs and errors

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var AWS = require('aws-sdk');
-var s3 = new AWS.S3({signatureVersion: 'v4'});
 var fs = require('fs');
 var chokidar = require('chokidar');
 var path = require('path');
 var zipdir = require('zip-dir');
+
+var AWS_BUCKET_NAME = 's3-link-generator';
+
+// Preparing s3 to new api version
+var s3 = new AWS.S3({signatureVersion: 'v4'});
 
 // Watch changes to a folder
 var folder_path = 'data';
@@ -14,32 +18,39 @@ var watcher = chokidar.watch(folder_path, {
 });
 watcher
     .on('add', function(new_path) {
+        // Only watching files of current folder
         if (path.dirname(new_path) === folder_path) {
             //console.log('File', new_path, 'has been added');
+            // Preparing S3 Upload
             var file_stream = fs.createReadStream(new_path);
-            var params = {Bucket: 's3-link-generator', Key: new_path, Body: file_stream};
+            var params = {Bucket: AWS_BUCKET_NAME, Key: new_path, Body: file_stream};
 
+            // Use progress listener as needed
             s3.upload(params).on('httpUploadProgress', function(event) {
                 //console.log('S3 upload progress:', event.loaded, '/', event.total);
             }).send(function(err, data) {
                 if (err) {
                     console.log("An error occurred ", err);
                 } else {
+                    // File upload done successfully
                     console.log("Uploaded the file at ", data.Location);
                 }
             });
         }
     })
     .on('addDir', function(new_path) {
+        // Only watching folder of current folder
         if (path.dirname(new_path) === folder_path) {
             //console.log('Directory', new_path, 'has been added');
+            // Zipping the folder in same location to be captured by file watcher
             zipdir(new_path, { saveTo: new_path + ".zip" }, function (err, buffer) {
                 // `buffer` is the buffer of the zipped file
-                // And the buffer was saved to `~/myzip.zip`
+                // And the buffer was saved to new_path + ".zip"
                 if (err) {
                     console.error('Error zipping ' + new_path);
                 } else {
-                    console.log('Successfully zipped ' + new_path + ".zip");
+                    // Successfully zipped the folder and now it will be caught by file watcher
+                    //console.log('Successfully zipped ' + new_path + ".zip");
                 }
             });
         }
