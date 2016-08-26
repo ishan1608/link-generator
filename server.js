5c809b80e6dafd8a764e00a83eb6763099fb4eb9
Ishan <ishanatmuzaffarpur@gmail.com>
2016-08-27 04:22:36 +0530

    zipping folders into same location so they can be catched by file watcher and uploaded to S3

--- server.js
+++ server.js
@@ -3,22 +3,7 @@ var s3 = new AWS.S3({signatureVersion: 'v4'});
 var fs = require('fs');
 var chokidar = require('chokidar');
 var path = require('path');
-
-/*// This uploads a file
-var file_name = 'acker7.jpg';
-var file_stream = fs.createReadStream('data/' + file_name);
-var file_key = file_name;
-var params = {Bucket: 's3-link-generator', Key: file_key, Body: file_stream};
-
-s3.upload(params).on('httpUploadProgress', function(event) {
-    console.log('Progress:', event.loaded, '/', event.total);
-}).send(function(err, data) {
-    if (err) {
-        console.log("An error occurred", err);
-    } else {
-        console.log("Uploaded the file at", data.Location);
-    }
-});*/
+var zipdir = require('zip-dir');
 
 // Watch changes to a folder
 var folder_path = 'data';
@@ -30,12 +15,12 @@ var watcher = chokidar.watch(folder_path, {
 watcher
     .on('add', function(new_path) {
         if (path.dirname(new_path) === folder_path) {
-            console.log('File', new_path, 'has been added');
+            //console.log('File', new_path, 'has been added');
             var file_stream = fs.createReadStream(new_path);
             var params = {Bucket: 's3-link-generator', Key: new_path, Body: file_stream};
 
             s3.upload(params).on('httpUploadProgress', function(event) {
-                console.log('S3 upload progress:', event.loaded, '/', event.total);
+                //console.log('S3 upload progress:', event.loaded, '/', event.total);
             }).send(function(err, data) {
                 if (err) {
                     console.log("An error occurred ", err);
@@ -47,6 +32,15 @@ watcher
     })
     .on('addDir', function(new_path) {
         if (path.dirname(new_path) === folder_path) {
-            console.log('Directory', new_path, 'has been added');
+            //console.log('Directory', new_path, 'has been added');
+            zipdir(new_path, { saveTo: new_path + ".zip" }, function (err, buffer) {
+                // `buffer` is the buffer of the zipped file
+                // And the buffer was saved to `~/myzip.zip`
+                if (err) {
+                    console.error('Error zipping ' + new_path);
+                } else {
+                    console.log('Successfully zipped ' + new_path + ".zip");
+                }
+            });
         }
     });
