eabf05f99e42f40279cd4373f80b444738ba61e2
Ishan <ishanatmuzaffarpur@gmail.com>
2016-08-27 04:05:02 +0530

    watching directories and uploading watched files

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var AWS = require('aws-sdk');
 var s3 = new AWS.S3({signatureVersion: 'v4'});
 var fs = require('fs');
+var chokidar = require('chokidar');
+var path = require('path');
 
+/*// This uploads a file
 var file_name = 'acker7.jpg';
 var file_stream = fs.createReadStream('data/' + file_name);
 var file_key = file_name;
@@ -15,4 +18,35 @@ s3.upload(params).on('httpUploadProgress', function(event) {
     } else {
         console.log("Uploaded the file at", data.Location);
     }
+});*/
+
+// Watch changes to a folder
+var folder_path = 'data';
+// Initialize watcher.
+var watcher = chokidar.watch(folder_path, {
+    ignored: /[\/\\]\./,
+    persistent: true
 });
+watcher
+    .on('add', function(new_path) {
+        if (path.dirname(new_path) === folder_path) {
+            console.log('File', new_path, 'has been added');
+            var file_stream = fs.createReadStream(new_path);
+            var params = {Bucket: 's3-link-generator', Key: new_path, Body: file_stream};
+
+            s3.upload(params).on('httpUploadProgress', function(event) {
+                console.log('S3 upload progress:', event.loaded, '/', event.total);
+            }).send(function(err, data) {
+                if (err) {
+                    console.log("An error occurred ", err);
+                } else {
+                    console.log("Uploaded the file at ", data.Location);
+                }
+            });
+        }
+    })
+    .on('addDir', function(new_path) {
+        if (path.dirname(new_path) === folder_path) {
+            console.log('Directory', new_path, 'has been added');
+        }
+    });
